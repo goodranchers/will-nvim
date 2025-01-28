43684b0d7853c5bf6f5e0b713b7bda65ffe3f7ed
wenglish-gr <wenglish@goodranchers.com>
2025-01-28 16:29:37 -0600

    keymaps for harpoon

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -132,3 +132,35 @@ vim.keymap.set(
     desc = "Code Actions",
   }
 )
+
+-- Require Harpoon modules
+local mark = require("harpoon.mark")
+local ui = require("harpoon.ui")
+
+-- Set keymaps for Harpoon
+vim.keymap.set(
+  "n",
+  "<leader>a",
+  mark.add_file,
+  { desc = "Harpoon: Add file" }
+) -- Add file to Harpoon
+vim.keymap.set(
+  "n",
+  "<leader>h",
+  ui.toggle_quick_menu,
+  { desc = "Harpoon: Toggle quick menu" }
+) -- Open Harpoon menu
+
+-- Navigate to files in Harpoon
+vim.keymap.set("n", "<leader>1", function()
+  ui.nav_file(1)
+end, { desc = "Harpoon: Go to file 1" })
+vim.keymap.set("n", "<leader>2", function()
+  ui.nav_file(2)
+end, { desc = "Harpoon: Go to file 2" })
+vim.keymap.set("n", "<leader>3", function()
+  ui.nav_file(3)
+end, { desc = "Harpoon: Go to file 3" })
+vim.keymap.set("n", "<leader>4", function()
+  ui.nav_file(4)
+end, { desc = "Harpoon: Go to file 4" })
